46f4abf7219a524ad20b3ffc9581dbe619233337
Maksim Blizhin <blizhinm@gmail.com>
2025-08-12 14:16:17 +0300

    [wezterm] macOS toggle opacity

--- wezterm/keys.lua
+++ wezterm/keys.lua
@@ -3,86 +3,96 @@ local action = wezterm.action
 local module = {}
 
 function module.apply_to_config(config)
-  config.leader = {
-    key = "Space",
-    mods = "CTRL|SHIFT",
-    timeout_milliseconds = 1000,
-  }
+	config.leader = {
+		key = "Space",
+		mods = "SHIFT|CTRL",
+		timeout_milliseconds = 1000,
+	}
 
-  config.keys = {
-    {
-      key = "v",
-      mods = "CTRL",
-      action = action.PasteFrom("Clipboard"),
-    },
-    { key = "UpArrow", mods = "SHIFT", action = action.ScrollByLine(-1) },
-    { key = "DownArrow", mods = "SHIFT", action = action.ScrollByLine(1) },
-    {
-      key = "w",
-      mods = "SHIFT|CTRL",
-      action = action.CloseCurrentTab({
-        confirm = false,
-      }),
-    },
-    {
-      key = "F11",
-      action = action.ToggleFullScreen,
-    },
-    {
-      key = "h",
-      mods = "LEADER",
-      action = action.SplitVertical({
-        domain = "CurrentPaneDomain",
-      }),
-    },
-    {
-      key = "v",
-      mods = "LEADER",
-      action = action.SplitHorizontal({
-        domain = "CurrentPaneDomain",
-      }),
-    },
-    {
-      key = "r",
-      mods = "LEADER",
-      action = action.RotatePanes("Clockwise"),
-    },
-    {
-      key = "h",
-      mods = "CTRL|ALT",
-      action = action.ActivatePaneDirection("Left"),
-    },
-    {
-      key = "l",
-      mods = "CTRL|ALT",
-      action = action.ActivatePaneDirection("Right"),
-    },
-    {
-      key = "k",
-      mods = "CTRL|ALT",
-      action = action.ActivatePaneDirection("Up"),
-    },
-    {
-      key = "j",
-      mods = "CTRL|ALT",
-      action = action.ActivatePaneDirection("Down"),
-    },
-  }
+	config.keys = {
+		{
+			key = "Space",
+			mods = "SHIFT|CTRL",
+			action = wezterm.action.DisableDefaultAssignment,
+		},
+		-- {
+		--   key = "v",
+		--   mods = "CTRL",
+		--   action = action.PasteFrom("Clipboard"),
+		-- },
+		{
+			key = "o",
+			mods = "LEADER",
+			action = action.EmitEvent("toggle-opacity"),
+		},
+		{ key = "UpArrow", mods = "SHIFT", action = action.ScrollByLine(-1) },
+		{ key = "DownArrow", mods = "SHIFT", action = action.ScrollByLine(1) },
+		{
+			key = "w",
+			mods = "SHIFT|CTRL",
+			action = action.CloseCurrentTab({
+				confirm = false,
+			}),
+		},
+		{
+			key = "F11",
+			action = action.ToggleFullScreen,
+		},
+		{
+			key = "v",
+			mods = "LEADER",
+			action = action.SplitVertical({
+				domain = "CurrentPaneDomain",
+			}),
+		},
+		{
+			key = "h",
+			mods = "LEADER",
+			action = action.SplitHorizontal({
+				domain = "CurrentPaneDomain",
+			}),
+		},
+		{
+			key = "r",
+			mods = "LEADER",
+			action = action.RotatePanes("Clockwise"),
+		},
+		{
+			key = "h",
+			mods = "CTRL|ALT",
+			action = action.ActivatePaneDirection("Left"),
+		},
+		{
+			key = "l",
+			mods = "CTRL|ALT",
+			action = action.ActivatePaneDirection("Right"),
+		},
+		{
+			key = "k",
+			mods = "CTRL|ALT",
+			action = action.ActivatePaneDirection("Up"),
+		},
+		{
+			key = "j",
+			mods = "CTRL|ALT",
+			action = action.ActivatePaneDirection("Down"),
+		},
+	}
 
-  -- config.key_tables = {
-  -- 	panes = {
-  -- 		{ key = "h", action = action.SplitVertical { domain = "CurrentPaneDomain" } },
-  -- 		{ key = "v", action = action.SplitHorizontal { domain = "CurrentPaneDomain" } },
+	-- config.key_tables = {
+	-- 	panes = {
+	-- 		{ key = "h", action = action.SplitVertical { domain = "CurrentPaneDomain" } },
+	-- 		{ key = "v", action = action.SplitHorizontal { domain = "CurrentPaneDomain" } },
 
-  --      { key = 'LeftArrow', action = action.AdjustPaneSize { 'Left', 1 } },
-  -- 		{ key = 'RightArrow', action = action.AdjustPaneSize { 'Right', 1 } },
-  -- 		{ key = 'UpArrow', action = action.AdjustPaneSize { 'Up', 1 } },
-  -- 		{ key = 'DownArrow', action = action.AdjustPaneSize { 'Down', 1 } },
+	--      { key = 'LeftArrow', action = action.AdjustPaneSize { 'Left', 1 } },
+	-- 		{ key = 'RightArrow', action = action.AdjustPaneSize { 'Right', 1 } },
+	-- 		{ key = 'UpArrow', action = action.AdjustPaneSize { 'Up', 1 } },
+	-- 		{ key = 'DownArrow', action = action.AdjustPaneSize { 'Down', 1 } },
 
-  -- 		-- Cancel the mode by pressing escape
-  -- 		{ key = "Escape", action = "PopKeyTable" },
-  -- 	},
-  -- }
+	-- 		-- Cancel the mode by pressing escape
+	-- 		{ key = "Escape", action = "PopKeyTable" },
+	-- 	},
+	-- }
 end
 
 return module
--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -9,37 +9,58 @@ local config = {}
 -- In newer versions of wezterm, use the config_builder which will
 -- help provide clearer error messages
 if wezterm.config_builder then
-  config = wezterm.config_builder()
+	config = wezterm.config_builder()
 end
 
 config.front_end = "OpenGL"
 config.max_fps = 60
 config.animation_fps = 30
 
-config.wsl_domains = {
-  {
-    -- The name of this specific domain.  Must be unique amonst all types
-    -- of domain in the configuration file.
-    name = "WSL:Ubuntu-22.04",
-
-    -- The name of the distribution.  This identifies the WSL distribution.
-    -- It must match a valid distribution from your `wsl -l -v` output in
-    -- order for the domain to be useful.
-    distribution = "Ubuntu-22.04",
-
-    -- 'default_cwd' for domain
-    default_cwd = "~",
-  },
-}
-config.default_domain = "WSL:Ubuntu-22.04"
+-- config.wsl_domains = {
+--   {
+--     -- The name of this specific domain.  Must be unique amonst all types
+--     -- of domain in the configuration file.
+--     name = "WSL:Ubuntu-22.04",
+--
+--     -- The name of the distribution.  This identifies the WSL distribution.
+--     -- It must match a valid distribution from your `wsl -l -v` output in
+--     -- order for the domain to be useful.
+--     distribution = "Ubuntu-22.04",
+--
+--     -- 'default_cwd' for domain
+--     default_cwd = "~",
+--   },
+-- }
+-- config.default_domain = "WSL:Ubuntu-22.04"
 
 config.window_close_confirmation = "NeverPrompt"
-config.skip_close_confirmation_for_processes_named =
-  { "bash", "sh", "zsh", "fish", "tmux" }
+config.skip_close_confirmation_for_processes_named = { "bash", "sh", "zsh", "fish", "tmux" }
 
 -- applying config modules
 keys.apply_to_config(config)
 style.apply_to_config(config)
+
+wezterm.on("toggle-opacity", function(window)
+	local overrides = window:get_config_overrides() or {}
+
+	if overrides.window_background_opacity == 1 then
+		style.background[1].opacity = 0.5
+		overrides.foreground_text_hsb = {
+			brightness = 0.75,
+		}
+		overrides.window_background_opacity = 0.5
+	else
+		style.background[1].opacity = 1
+		overrides.foreground_text_hsb = {
+			brightness = 1,
+		}
+		overrides.window_background_opacity = 1
+	end
+
+	overrides.background = style.background
+
+	window:set_config_overrides(overrides)
+end)
 
 -- and finally, return the configuration to wezterm
 return config
