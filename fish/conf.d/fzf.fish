07a974248c3ee8ae572f2053d9dafcb1e221dc65
Maksim Blizhin <blizhinm@gmail.com>
2025-09-08 14:34:45 +0300

    [fzf] updated colorscheme to suit 'catppuccin mocha'

--- fish/conf.d/fzf.fish
+++ fish/conf.d/fzf.fish
@@ -7,6 +7,31 @@ set -x FZF_ALT_C_COMMAND "fd --type=d --hidden --strip-cwd-prefix --exclude .git
 set -x FZF_CTRL_T_OPTS "--preview 'bat -n --line-range :500 {}'"
 set -x FZF_ALT_C_OPTS "--preview 'eza --icons=always --tree --color=always {} | head -200'"
 
+# set -x FZF_DEFAULT_OPTS "$FZF_DEFAULT_OPTS \
+#   --height=90% \
+#   --highlight-line \
+#   --info=inline-right \
+#   --ansi \
+#   --border \
+#   --layout=default \
+#   --color=bg+:#283457 \
+#   --color=bg:-1 \
+#   --color=border:#27a1b9 \
+#   --color=fg:#c0caf5 \
+#   --color=gutter:#16161e \
+#   --color=header:#ff9e64 \
+#   --color=hl+:#2ac3de \
+#   --color=hl:#2ac3de \
+#   --color=info:#545c7e \
+#   --color=marker:#ff007c \
+#   --color=pointer:#ff007c \
+#   --color=prompt:#2ac3de \
+#   --color=query:#c0caf5:regular \
+#   --color=scrollbar:#27a1b9 \
+#   --color=separator:#ff9e64 \
+#   --color=spinner:#ff007c \
+# "
+
 set -x FZF_DEFAULT_OPTS "$FZF_DEFAULT_OPTS \
   --height=90% \
   --highlight-line \
@@ -14,20 +39,22 @@ set -x FZF_DEFAULT_OPTS "$FZF_DEFAULT_OPTS \
   --ansi \
   --border \
   --layout=default \
-  --color=bg+:#283457 \
-  --color=bg:-1 \
-  --color=border:#27a1b9 \
-  --color=fg:#c0caf5 \
-  --color=gutter:#16161e \
-  --color=header:#ff9e64 \
-  --color=hl+:#2ac3de \
-  --color=hl:#2ac3de \
-  --color=info:#545c7e \
-  --color=marker:#ff007c \
-  --color=pointer:#ff007c \
-  --color=prompt:#2ac3de \
-  --color=query:#c0caf5:regular \
-  --color=scrollbar:#27a1b9 \
-  --color=separator:#ff9e64 \
-  --color=spinner:#ff007c \
+	--color=bg:-1 \
+  --color=bg+:#313244 \
+	--color=spinner:#F5E0DC \
+	--color=hl:#f5c2e7 \
+  --color=hl+:#f5c2e7 \
+  --color=fg:#CDD6F4 \
+  --color=fg+:#CDD6F4 \
+	--color=header:#F38BA8 \
+	--color=info:#94e2d5 \
+	--color=pointer:#CBA6F7 \
+  --color=marker:#B4BEFE \
+	--color=prompt:#eba0ac \
+	--color=query:#CDD6F4 \
+  --color=selected-bg:#45475A \
+  --color=border:#fab387 \
+	--color=label:#CDD6F4 \
+  --color=separator:#CBA6F7 \
+  --color=gutter:-1 \
 "
